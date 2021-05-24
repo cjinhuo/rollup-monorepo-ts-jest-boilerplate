b043a8a66fe2e39056be6f32f6a87d0cb8f53ee9
jinhuochen <1443970739@qq.com>
2021-05-24 15:20:44 +0800

    test(core.test): add sub function test

--- test/unit/core/index.spec.ts
+++ test/unit/core/index.spec.ts
@@ -1,7 +1,10 @@
-import { add } from '@boilerplate/core'
+import { add, sub } from '@boilerplate/core'
 
 describe('core idnex.ts', () => {
   it('add func should be work', () => {
     expect(add(1, 2)).toBe(3)
+  })
+  it('sub func should be work', () => {
+    expect(sub(2, 1)).toBe(1)
   })
 })
