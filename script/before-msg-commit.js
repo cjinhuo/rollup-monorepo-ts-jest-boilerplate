a3288d5e3259893b0e7d7a1eb23b852700d33b3b
jinhuochen <1443970739@qq.com>
2021-05-24 11:49:56 +0800

    chore(.husky): temp store

--- script/before-msg-commit.js
+++ script/before-msg-commit.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const gitRootDir = __dirname + '/../'
 console.log('process.env.HUSKY_GIT_PARAMS', process.env.$1, process.env)
+// 拿到git commit message 判断是否是chore(package.version)，是的话就执行 npm run initChangelog
 if (process.env.HUSKY_GIT_PARAMS) {
   const messageFile = path.normalize(gitRootDir + '/' + process.env.HUSKY_GIT_PARAMS.split(' ')[0])
   let commitMessage = `\nproject commit:\n`
